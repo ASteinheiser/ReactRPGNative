b47fe36632af7c088e56c9ccf0e548025cdbb0f7
Andrew Steinheiser <me@iamandrew.io>
2019-01-28 09:35:31 -0700

    add cookie manager to refresh webview cache for new updates

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { StatusBar, View }  from 'react-native';
+import CookieManager        from 'react-native-cookies';
 import SplashScreen         from 'react-native-splash-screen';
 import { WebView }          from 'react-native-webview';
+
+const GAME_URL = 'http://react-rpg.com';
 
 export default class App extends Component {
 
   componentWillMount() {
+    // clear the cookies on mount to refresh the webview
+    CookieManager.clearAll().then(console.log).catch(console.error);
     // show splash screen for 1.5 sec
     setTimeout(() => SplashScreen.hide(), 1500);
   }
@@ -15,7 +20,7 @@ export default class App extends Component {
       <View style={{height: '100%'}}>
         <StatusBar hidden />
 
-        <WebView source={{uri: 'http://react-rpg.com'}} />
+        <WebView source={{uri: GAME_URL}} />
       </View>
     );
   }
--- android/app/src/main/java/com/reactrpgnative/MainApplication.java
+++ android/app/src/main/java/com/reactrpgnative/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactrpgnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.psykar.cookiemanager.CookieManagerPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CookieManagerPackage(),
             new SplashScreenReactPackage(),
             new RNCWebViewPackage()
       );
